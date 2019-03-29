1a3b89531c0682bb11c59a0233bddae396ec4eb0
Alex Korostylev <ac.korostylev@gmail.com>
2019-03-29 12:26:36 +0300

    switch off FloatingButton

--- app/src/main/java/spain/barcelona/mydraw/MainActivity.java
+++ app/src/main/java/spain/barcelona/mydraw/MainActivity.java
@@ -1,5 +1,6 @@
 package spain.barcelona.mydraw;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -25,14 +26,14 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -42,6 +43,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
     }
 
     @Override
@@ -83,6 +86,7 @@ public class MainActivity extends AppCompatActivity
         // Создадим новый фрагмент
         Fragment fragment = null;
         Class fragmentClass = null;
+
 
         // Handle navigation view item clicks here.
         int id = item.getItemId();
