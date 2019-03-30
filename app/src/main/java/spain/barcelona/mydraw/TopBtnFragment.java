2ebad31d58ad9bd851d4646cc783ed6f450edf24
Alex Korostylev <ac.korostylev@gmail.com>
2019-03-30 17:18:13 +0300

    new Structure. add TopFragment with TopImgFragment, TopBtnFragment iside

---
+++ app/src/main/java/spain/barcelona/mydraw/TopBtnFragment.java
@@ -0,0 +1,66 @@
+
+package spain.barcelona.mydraw;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class TopBtnFragment extends Fragment implements View.OnClickListener {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_top_btn, container, false);
+        // Inflate the layout for this fragment
+
+        Button btnNext = v.findViewById(R.id.btn_next);
+        btnNext.setOnClickListener(this);
+        Button btnPrevious = v.findViewById(R.id.btn_previous);
+        btnPrevious.setOnClickListener(this);
+
+        return v;
+    }
+
+
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_next:
+                onClickNext(v);
+                break;
+            case R.id.btn_previous:
+                onClickPrevious(v);
+                break;
+
+        }
+    }
+    public void onClickNext(View v) {
+        //Spinner artType = (Spinner) view.findViewById(R.id.art_type);
+        //String spinnerMsg = (String)artType.getSelectedItem();
+        ImageView image = getActivity().findViewById(R.id.info_image);
+        image.setImageResource(R.drawable.p19);
+        TextView nameText = getActivity().findViewById(R.id.name_text);
+        nameText.setText(R.string.p19_name);
+        TextView detailText = getActivity().findViewById(R.id.detail_text);
+        detailText.setText(R.string.p19_detail);
+        //TextView lotNum = (TextView)getActivity().findViewById(R.id.lot_num);
+        //lotNum.setText();
+    }
+
+    public void onClickPrevious(View v) {
+        ImageView image = getActivity().findViewById(R.id.info_image);
+        image.setImageResource(R.drawable.s11_p);
+        TextView nameText = getActivity().findViewById(R.id.name_text);
+        nameText.setText(R.string.s11_p_name);
+        TextView detailText = getActivity().findViewById(R.id.detail_text);
+        detailText.setText(R.string.s11_p_detail);
+    }
+
+}
---
+++ app/src/main/java/spain/barcelona/mydraw/TopImgFragment.java
@@ -0,0 +1,23 @@
+package spain.barcelona.mydraw;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class TopImgFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        return inflater.inflate(R.layout.fragment_top_img, container, false);
+    }
+
+}
