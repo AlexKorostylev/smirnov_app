bc4cd882bdb20157cfadfe2fd65d747b677248e9
Alex Korostylev <ac.korostylev@gmail.com>
2019-05-29 20:35:42 +0300

    new version of Biography branch (with 1 fragment)

--- app/src/main/java/spain/barcelona/mydraw/BioFragment.java
+++ app/src/main/java/spain/barcelona/mydraw/BioFragment.java
@@ -1,9 +1,11 @@
 package spain.barcelona.mydraw;
 
+import android.app.FragmentTransaction;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +19,11 @@ public class BioFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_bio, container, false);
+
+        Toolbar toolbar = getActivity().findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.drawer_bio);
+
+        return inflater.inflate(R.layout.fragment_biogrphy, container, false);
+        //return inflater.inflate(R.layout.fragment_bio, container, false);
     }
 }
