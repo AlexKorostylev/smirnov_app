3e99b2c99ae2122a1093451b070f094d0f6d9bb2
Alex Korostylev <ac.korostylev@gmail.com>
2019-06-15 16:36:03 +0300

    delete Floating Action button

--- app/src/main/java/spain/barcelona/mydraw/MainActivity.java
+++ app/src/main/java/spain/barcelona/mydraw/MainActivity.java
@@ -23,15 +23,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -42,8 +33,6 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
         // Фрагмент открывающийся при начальной загрузке
-
-
         TopFragment topFragment = new TopFragment();
         FragmentTransaction ft = getFragmentManager().beginTransaction();
         ft.replace(R.id.top_container, topFragment);
