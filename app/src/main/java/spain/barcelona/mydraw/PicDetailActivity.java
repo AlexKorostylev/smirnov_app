e60c3cadb932771dd3b432e64ee5c69b7fdc31a8
Alex Korostylev <ac.korostylev@gmail.com>
2019-11-04 19:31:48 +0300

    activity and layout for Picture Detail

---
+++ app/src/main/java/spain/barcelona/mydraw/PicDetailActivity.java
@@ -0,0 +1,14 @@
+package spain.barcelona.mydraw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PicDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pic_detail);
+
+    }
+}
