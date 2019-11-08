de4eef291b53c69fac9854899616b1767b9aa867
Alex Korostylev <ac.korostylev@gmail.com>
2019-11-08 22:46:11 +0300

    PicDetailCard ready

--- app/src/main/java/spain/barcelona/mydraw/PicDetailActivity.java
+++ app/src/main/java/spain/barcelona/mydraw/PicDetailActivity.java
@@ -2,6 +2,10 @@ package spain.barcelona.mydraw;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import static spain.barcelona.mydraw.Pic.paint;
 
 public class PicDetailActivity extends AppCompatActivity {
 
@@ -9,6 +13,24 @@ public class PicDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pic_detail);
+
+        ImageView picImg = findViewById(R.id.detail_pic_img);
+        TextView picName = findViewById(R.id.detail_pic_name); // Название картины
+        TextView picMaterial = findViewById(R.id.detail_pic_material); // Техника исполнения (Например: холст, масло)
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            int picIndex = bundle.getInt("PIC_INDEX");
+            //String paramFromIntent = Integer.toString(picIndex);
+            picImg.setImageResource(paint[picIndex].getImageResourceId());
+            picName.setText(paint[picIndex].getName());
+
+            String picDetail = paint[picIndex].getMaterial() + "   " + (paint[picIndex].getSize()) + "   " +
+                    (paint[picIndex].getYear())+"г.";
+            picMaterial.setText(picDetail);
+        }
+
+
 
     }
 }
