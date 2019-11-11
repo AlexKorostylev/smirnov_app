5e77f9860e673b5db691d9b6e6a6f0bd4718ef12
Alex Korostylev <ac.korostylev@gmail.com>
2019-11-11 06:32:22 +0300

    increasing\decreasing text at Interview screen

--- app/src/main/java/spain/barcelona/mydraw/InterviewDetailFragment.java
+++ app/src/main/java/spain/barcelona/mydraw/InterviewDetailFragment.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 public class InterviewDetailFragment extends Fragment {
 
     private long interviewId;
+    public static int fontSizeChange = 4;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -48,12 +49,117 @@ public class InterviewDetailFragment extends Fragment {
             TextView title = view.findViewById(R.id.title_text);
             Text interview = Text.interview[(int) interviewId];
             title.setText(interview.getTitle());
-            TextView description = view.findViewById(R.id.description_text);
+            final TextView description = view.findViewById(R.id.description_text);
             description.setText(interview.getDescription());
 
-            TextView sign = view.findViewById(R.id.outputData_text);
+            final TextView sign = view.findViewById(R.id.outputData_text);
             String outputData = interview.getAuthor() + ". \n" + interview.getOutputData();
             sign.setText(outputData);
+
+            if (fontSizeChange == 1) {
+                description.setTextSize(getResources().getDimension(R.dimen.text_body_11));
+                sign.setTextSize(getResources().getDimension(R.dimen.text_body_11));
+            } else if (fontSizeChange == 2) {
+                description.setTextSize(getResources().getDimension(R.dimen.text_body_12));
+                sign.setTextSize(getResources().getDimension(R.dimen.text_body_12));
+            } else if (fontSizeChange == 3) {
+                description.setTextSize(getResources().getDimension(R.dimen.text_body_13));
+                sign.setTextSize(getResources().getDimension(R.dimen.text_body_13));
+            } else if (fontSizeChange == 4) {
+                description.setTextSize(getResources().getDimension(R.dimen.text_body_14));
+                sign.setTextSize(getResources().getDimension(R.dimen.text_body_14));
+            } else if (fontSizeChange == 5) {
+                description.setTextSize(getResources().getDimension(R.dimen.text_body_15));
+                sign.setTextSize(getResources().getDimension(R.dimen.text_body_15));
+            } else if (fontSizeChange == 6) {
+                description.setTextSize(getResources().getDimension(R.dimen.text_body_16));
+                sign.setTextSize(getResources().getDimension(R.dimen.text_body_16));
+            } else if (fontSizeChange == 7) {
+                description.setTextSize(getResources().getDimension(R.dimen.text_body_17));
+                sign.setTextSize(getResources().getDimension(R.dimen.text_body_17));
+            } else if (fontSizeChange == 8) {
+                description.setTextSize(getResources().getDimension(R.dimen.text_body_18));
+                sign.setTextSize(getResources().getDimension(R.dimen.text_body_18));
+            } else if (fontSizeChange == 9) {
+                description.setTextSize(getResources().getDimension(R.dimen.text_body_19));
+                sign.setTextSize(getResources().getDimension(R.dimen.text_body_19));
+            }
+
+            // Increase button
+            ImageView increaseBtn = view.findViewById(R.id.font_increase_btn);
+            increaseBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ++fontSizeChange;
+                    if (fontSizeChange == 1) {
+                        description.setTextSize(getResources().getDimension(R.dimen.text_body_11));
+                        sign.setTextSize(getResources().getDimension(R.dimen.text_body_11));
+                    } else if (fontSizeChange == 2) {
+                        description.setTextSize(getResources().getDimension(R.dimen.text_body_12));
+                        sign.setTextSize(getResources().getDimension(R.dimen.text_body_12));
+                    } else if (fontSizeChange == 3) {
+                        description.setTextSize(getResources().getDimension(R.dimen.text_body_13));
+                        sign.setTextSize(getResources().getDimension(R.dimen.text_body_13));
+                    } else if (fontSizeChange == 4) {
+                        description.setTextSize(getResources().getDimension(R.dimen.text_body_14));
+                        sign.setTextSize(getResources().getDimension(R.dimen.text_body_14));
+                    } else if (fontSizeChange == 5) {
+                        description.setTextSize(getResources().getDimension(R.dimen.text_body_15));
+                        sign.setTextSize(getResources().getDimension(R.dimen.text_body_15));
+                    } else if (fontSizeChange == 6) {
+                        description.setTextSize(getResources().getDimension(R.dimen.text_body_16));
+                        sign.setTextSize(getResources().getDimension(R.dimen.text_body_16));
+                    } else if (fontSizeChange == 7) {
+                        description.setTextSize(getResources().getDimension(R.dimen.text_body_17));
+                        sign.setTextSize(getResources().getDimension(R.dimen.text_body_17));
+                    } else if (fontSizeChange == 8) {
+                        description.setTextSize(getResources().getDimension(R.dimen.text_body_18));
+                        sign.setTextSize(getResources().getDimension(R.dimen.text_body_18));
+                    } else if (fontSizeChange >= 9) {
+                        description.setTextSize(getResources().getDimension(R.dimen.text_body_19));
+                        sign.setTextSize(getResources().getDimension(R.dimen.text_body_19));
+                        fontSizeChange = 9;
+                    }
+                }
+            });
+
+            // Decrease button
+            ImageView decreaseBtn = view.findViewById(R.id.font_decrease_btn);
+            decreaseBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    --fontSizeChange;
+                    if (fontSizeChange <= 1) {
+                        description.setTextSize(getResources().getDimension(R.dimen.text_body_11));
+                        sign.setTextSize(getResources().getDimension(R.dimen.text_body_11));
+                        fontSizeChange = 1;
+                    } else if (fontSizeChange == 2) {
+                        description.setTextSize(getResources().getDimension(R.dimen.text_body_12));
+                        sign.setTextSize(getResources().getDimension(R.dimen.text_body_12));
+                    } else if (fontSizeChange == 3) {
+                        description.setTextSize(getResources().getDimension(R.dimen.text_body_13));
+                        sign.setTextSize(getResources().getDimension(R.dimen.text_body_13));
+                    } else if (fontSizeChange == 4) {
+                        description.setTextSize(getResources().getDimension(R.dimen.text_body_14));
+                        sign.setTextSize(getResources().getDimension(R.dimen.text_body_14));
+                    } else if (fontSizeChange == 5) {
+                        description.setTextSize(getResources().getDimension(R.dimen.text_body_15));
+                        sign.setTextSize(getResources().getDimension(R.dimen.text_body_15));
+                    } else if (fontSizeChange == 6) {
+                        description.setTextSize(getResources().getDimension(R.dimen.text_body_16));
+                        sign.setTextSize(getResources().getDimension(R.dimen.text_body_16));
+                    } else if (fontSizeChange == 7) {
+                        description.setTextSize(getResources().getDimension(R.dimen.text_body_17));
+                        sign.setTextSize(getResources().getDimension(R.dimen.text_body_17));
+                    } else if (fontSizeChange == 8) {
+                        description.setTextSize(getResources().getDimension(R.dimen.text_body_18));
+                        sign.setTextSize(getResources().getDimension(R.dimen.text_body_18));
+                    } else if (fontSizeChange == 9) {
+                        description.setTextSize(getResources().getDimension(R.dimen.text_body_19));
+                        sign.setTextSize(getResources().getDimension(R.dimen.text_body_19));
+                    }
+                }
+            });
         }
     }
 
