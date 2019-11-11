6e07c2532b540873ec2595eb09474d1e3d5df547
Alex Korostylev <ac.korostylev@gmail.com>
2019-11-11 06:30:18 +0300

    code refactoring

--- app/src/main/java/spain/barcelona/mydraw/AboutDetailFragment.java
+++ app/src/main/java/spain/barcelona/mydraw/AboutDetailFragment.java
@@ -2,7 +2,6 @@ package spain.barcelona.mydraw;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
