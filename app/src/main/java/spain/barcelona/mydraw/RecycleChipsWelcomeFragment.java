617b68ed8a80209a34f6c8706c59ffbf6abc8e6f
Alex Korostylev <ac.korostylev@gmail.com>
2019-11-18 23:30:13 +0300

    add layout for welcome screen

--- app/src/main/java/spain/barcelona/mydraw/RecycleChipsWelcomeFragment.java
+++ app/src/main/java/spain/barcelona/mydraw/RecycleChipsWelcomeFragment.java
@@ -30,33 +30,23 @@ public class RecycleChipsWelcomeFragment extends Fragment implements View.OnClic
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        View v = inflater.inflate(R.layout.fragment_recycle_chips, container, false);
+        View v = inflater.inflate(R.layout.fragment_recycle_chips_welcome, container, false);
 
-        Button chipBtnAll = v.findViewById(R.id.chipBtnAll);
-        chipBtnAll.setText(R.string.chip_btn_welcome);
-        chipBtnAll.setOnClickListener(this);
+
         Button chipBtn_1 = v.findViewById(R.id.chipBtn_1);
-        chipBtn_1.setText(R.string.chip_btn_art_paint);
+        chipBtn_1.setText(R.string.chip_btn_welcome);
         chipBtn_1.setOnClickListener(this);
         Button chipBtn_2 = v.findViewById(R.id.chipBtn_2);
-        chipBtn_2.setText(R.string.chip_btn_art_graphic);
+        chipBtn_2.setText(R.string.chip_btn_art_paint);
         chipBtn_2.setOnClickListener(this);
         Button chipBtn_3 = v.findViewById(R.id.chipBtn_3);
-        chipBtn_3.setVisibility(View.INVISIBLE);
-        Button chipBtn_4 = v.findViewById(R.id.chipBtn_4);
-        chipBtn_4.setVisibility(View.INVISIBLE);
-
-
+        chipBtn_3.setText(R.string.chip_btn_art_graphic);
 
         return v;
     }
 
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.chipBtnAll:
-                periodCurrentStateWelcome =0;
-                chipListener.chipClick();
-                break;
             case R.id.chipBtn_1:
                 periodCurrentStateWelcome =1;
                 chipListener.chipClick();
@@ -69,10 +59,7 @@ public class RecycleChipsWelcomeFragment extends Fragment implements View.OnClic
                 periodCurrentStateWelcome =3;
                 chipListener.chipClick();
                 break;
-            case R.id.chipBtn_4:
-                periodCurrentStateWelcome =4;
-                chipListener.chipClick();
-                break;
+
         }
     }
 
