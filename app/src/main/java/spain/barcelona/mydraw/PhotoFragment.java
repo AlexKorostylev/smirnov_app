86fb69851ce271a0b598b1ab065ccf80ee97311f
Alex Korostylev <ac.korostylev@gmail.com>
2019-11-19 02:30:11 +0300

    make action on the button Photo screen

--- app/src/main/java/spain/barcelona/mydraw/PhotoFragment.java
+++ app/src/main/java/spain/barcelona/mydraw/PhotoFragment.java
@@ -1,21 +1,40 @@
 package spain.barcelona.mydraw;
 
 
+import android.app.Activity;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import static spain.barcelona.mydraw.MainActivity.appBranch;
 
 public class PhotoFragment extends Fragment {
 
+    ChipListener chipListener;
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            chipListener = (ChipListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " must implement onClickChipListener");
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        setHasOptionsMenu(true);
+
         FragmentTransaction ft = getChildFragmentManager().beginTransaction();
         PhotoImgFragment imgFragment = new PhotoImgFragment();
         PhotoBtnFragment btnFragment = new PhotoBtnFragment();
@@ -30,6 +49,33 @@ public class PhotoFragment extends Fragment {
         ((MainActivity)getActivity()).getSupportActionBar().show();
 
         return inflater.inflate(R.layout.fragment_photo, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Do something that differs the Activity's menu here
+        menu.findItem(R.id.recycle_list).setTitle("Frame");
+        menu.findItem(R.id.recycle_list).setIcon(R.drawable.frame_grid);
+        super.onCreateOptionsMenu(menu, inflater);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.recycle_list:
+                if (item.getTitle().equals("Frame")) {
+                    RecycleTopFragment recycleTopFragment = new RecycleTopFragment();
+                    FragmentTransaction ft = getFragmentManager().beginTransaction();
+                    ft.replace(R.id.top_container, recycleTopFragment);
+                    ft.addToBackStack(null);
+                    ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                    ft.commit();
+                    MainActivity.startPositionPhoto = 1;
+                    appBranch = "photoBranch";
+                }
+        }
+        return false;
     }
 
 }
