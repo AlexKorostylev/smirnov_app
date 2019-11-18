d1d9ec6992e3aac4fecb261edd4a89dac315ff63
Alex Korostylev <ac.korostylev@gmail.com>
2019-11-18 20:13:31 +0300

    add Graphic Recycle fragments

--- app/src/main/java/spain/barcelona/mydraw/RecycleTopFragment.java
+++ app/src/main/java/spain/barcelona/mydraw/RecycleTopFragment.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
+import static spain.barcelona.mydraw.MainActivity.appBranch;
 
 public class RecycleTopFragment extends Fragment {
 
@@ -23,18 +24,38 @@ public class RecycleTopFragment extends Fragment {
 
         setHasOptionsMenu(true);
 
-        RecycleListFragment recycleListFragment = new RecycleListFragment();
-        RecycleChipsFragment recycleChipsFragment = new RecycleChipsFragment();
-        FragmentTransaction ft = getChildFragmentManager().beginTransaction();
+        if(appBranch.equals("graphicBranch")){
+            RecycleListGraphicFragment recycleListGraphic = new RecycleListGraphicFragment();
+            RecycleChipsGraphicFragment recycleChipsFragment = new RecycleChipsGraphicFragment();
+            FragmentTransaction ft = getChildFragmentManager().beginTransaction();
 
-        ft.replace(R.id.recycle_chips_container, recycleChipsFragment);
-        ft.replace(R.id.recycle_list_container, recycleListFragment);
-        ft.addToBackStack(null);
-        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-        ft.commit();
+            ft.replace(R.id.recycle_chips_container, recycleChipsFragment);
+            ft.replace(R.id.recycle_list_container, recycleListGraphic);
+            ft.addToBackStack(null);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.commit();
 
-        Toolbar toolbar = getActivity().findViewById(R.id.toolbar);
-        toolbar.setTitle("Живопись");
+            Toolbar toolbar = getActivity().findViewById(R.id.toolbar);
+            toolbar.setTitle("Графика");
+
+        } else if(appBranch.equals("paintingBranch")){
+            RecycleListPaintFragment recycleListFragment = new RecycleListPaintFragment();
+            RecycleChipsPaintFragment recycleChipsFragment = new RecycleChipsPaintFragment();
+            FragmentTransaction ft = getChildFragmentManager().beginTransaction();
+
+            ft.replace(R.id.recycle_chips_container, recycleChipsFragment);
+            ft.replace(R.id.recycle_list_container, recycleListFragment);
+            ft.addToBackStack(null);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.commit();
+
+            Toolbar toolbar = getActivity().findViewById(R.id.toolbar);
+            toolbar.setTitle("Живопись");
+        }
+
+
+
+
 
         ((MainActivity) getActivity()).getSupportActionBar().show();
 
@@ -56,13 +77,25 @@ public class RecycleTopFragment extends Fragment {
             case R.id.recycle_list:
                 item.setTitle("Frame");
                 item.setIcon(R.drawable.frame_grid);
-                PaintFragment paintingFragment = new PaintFragment();
-                FragmentTransaction ft = getFragmentManager().beginTransaction();
-                ft.replace(R.id.top_container, paintingFragment);
-                ft.addToBackStack(null);
-                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-                ft.commit();
-                MainActivity.startPosition = 0;
+                if(appBranch.equals("graphicBranch")){
+                    GraphicFragment graphicFragment = new GraphicFragment();
+                    FragmentTransaction ft = getFragmentManager().beginTransaction();
+                    ft.replace(R.id.top_container, graphicFragment);
+                    ft.addToBackStack(null);
+                    ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                    ft.commit();
+                    MainActivity.startPositionPaint = 0;
+                } else if(appBranch.equals("paintingBranch")) {
+                    PaintFragment paintingFragment = new PaintFragment();
+                    FragmentTransaction ft = getFragmentManager().beginTransaction();
+                    ft.replace(R.id.top_container, paintingFragment);
+                    ft.addToBackStack(null);
+                    ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                    ft.commit();
+                    MainActivity.startPositionPaint = 0;
+                }
+
+
                 break;
             case R.id.background_mode:
                 FrameLayout layout = getActivity().findViewById(R.id.recycle_chips_container);
